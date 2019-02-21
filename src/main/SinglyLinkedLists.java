711b739fbac1c32ba15d756becc2f894687d8d4c
Gaganvarshney97 <gagan.varshney_ccv17@gla.ac.in>
2019-02-21 08:31:27 +0530

    Add constructor

---
+++ src/main/SinglyLinkedLists.java
@@ -0,0 +1,20 @@
+package main;
+
+
+public class SinglyLinkedLists{
+    private static class Node{
+        private int data;
+        private Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next= null;
+        }
+
+        public Node(int data, Node next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+}
